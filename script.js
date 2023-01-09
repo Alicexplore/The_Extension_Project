7f24188c933b110cc2eb6c367a4e8ac005b9edd2
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-01-09 14:32:59 +0100

    last versions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const url = `https://dog.ceo/api/breeds/image/random`;
+async function getDogPhoto() {
+    const result = await fetch(url);
+    result.json().then(json => {
+    console.log(json.message);
+    document.getElementById("displayPhoto").src = json.message
+    })   
+}
+getDogPhoto();
+
+// document.getElementById("displayPhoto").innerHTML = json.message .value
+document.getElementById("displayPhoto").src = json.message
