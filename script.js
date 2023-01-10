9bfdff5bdf4b9698ab27bd98b9c00435b1b360fe
HortenseLaclaviere <114992888+HortenseLaclaviere@users.noreply.github.com>
2023-01-10 14:11:31 +0100

    hortense

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ async function getDogPhoto() {
 getDogPhoto();
 
 // document.getElementById("displayPhoto").innerHTML = json.message .value
+
+
+//Génère un entier aléatoire pour proposer un ID d'oeuvre
+function entierAleatoire(min, max)
+{ return Math.floor(Math.random() * (max - min + 1)) + min;}
+var entier = entierAleatoire(1, 471581);
+
+
+
+
+const url2 = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"+ entier;
+async function getArt(){
+    const result = await fetch(url2);
+    result.json().then(json => {
+        console.log(json);
+        document.getElementById("art").innerHTML = json.title + " " + json.artistDisplayName + " " + json.period;
+        document.getElementById("displayPhoto2").src = json.primaryImage;
+    }
+
+    )
+}
+getArt();
