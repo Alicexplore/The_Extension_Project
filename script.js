40e54ab030b82fb057d3daaa2068a6ce79397803
HortenseLaclaviere <114992888+HortenseLaclaviere@users.noreply.github.com>
2023-01-10 16:41:04 +0100

    hortense

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 
-const url = `https://dog.ceo/api/breeds/image/random`;
-async function getDogPhoto() {
-    const result = await fetch(url);
-    result.json().then(json => {
-    console.log(json.message);
-    document.getElementById("displayPhoto").src = json.message
-    })   
-}
-getDogPhoto();
-
-// document.getElementById("displayPhoto").innerHTML = json.message .value
+// const url = `https://dog.ceo/api/breeds/image/random`;
+// async function getDogPhoto() {
+//     const result = await fetch(url);
+//     result.json().then(json => {
+//     console.log(json.message);
+//     document.getElementById("displayPhoto").src = json.message
+//     })   
+// }
+// getDogPhoto();
 
 
 //Génère un entier aléatoire pour proposer un ID d'oeuvre
-function entierAleatoire(min, max)
-{ return Math.floor(Math.random() * (max - min + 1)) + min;}
-var entier = entierAleatoire(1, 471581);
+// function entierAleatoire(min, max)
+// { return Math.floor(Math.random() * (max - min + 1)) + min;}
+// var entier = entierAleatoire(1, 471581);
 
 
-
-
-const url2 = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"+ entier;
-async function getArt(){
-    const result = await fetch(url2);
-    result.json().then(json => {
+async function getArt() {
+    while (true) {
+      let id = Math.floor(Math.random() * 50000) + 1;
+      const url2 = "https://collectionapi.metmuseum.org/public/collection/v1/objects/" + id;
+      const result = await fetch(url2);
+      const json = await result.json();
+      if (json.primaryImage) {
         console.log(json);
-        document.getElementById("art").innerHTML = json.title + " " + json.artistDisplayName + " " + json.period;
+        document.getElementById("artTitle").innerHTML = json.title;
+        document.getElementById("art").innerHTML = json.artistDisplayName + " " + json.period;
         document.getElementById("displayPhoto2").src = json.primaryImage;
+        break;
+      }
     }
-
-    )
-}
-getArt();
+  }
+  getArt();
