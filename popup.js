7681f6459c98a90f8dbc4bbd6a997a1f488c8dea
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-01-17 14:06:20 +0100

    alice

---
+++ popup.js
@@ -0,0 +1,67 @@
+/////////////////////////////////////////////////////// create close a button  ///////////////////////////////////////////////////////
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+/////////////////////////////////////////////////////// close a button  /////////////////////////////////////////////////////////////
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+/////////////////////////////////////////////////////// checked symbol //////////////////////////////////////////////////////////////
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+/////////////////////////////////////////////////////// clicking on add button //////////////////////////////////////////////////////
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    // alert("Ready to write your next  ");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+  document.getElementById("newElement").addEventListener("click", newElement);
+};
+
+newElement();
+
+
+
+
+
